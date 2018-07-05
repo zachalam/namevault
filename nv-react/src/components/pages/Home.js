30414e9902872f5adb02c0c9ecf5e361def0fa67
Zach Alam <zelphics@gmail.com>
2018-07-05 00:39:23 -0700

    layout

--- nv-react/src/components/pages/Home.js
+++ nv-react/src/components/pages/Home.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
-import { Input } from 'semantic-ui-react'
+import { Input, Card } from 'semantic-ui-react'
 import logo from '../../images/logo.png'
+import FadeIn from 'react-fade-in'
 
 class Home extends Component {
 
     state = {
-        searchTerm: ''
+        searchTerm: '',
+        searchLoading: false
     }
     
     onSearchChange = (e) => {
         // only allow alphanumeric chars.
-        var letters = /^[0-9a-zA-Z]+$/;
-        if(e.target.value.match(letters) || e.target.value === '') {
-            this.setState({searchTerm: e.target.value})
+        let letters = /^[0-9a-zA-Z]+$/;
+        let { value } = e.target;
+
+        if(value.match(letters) || value === '') {
+            // save search term so far.
+            this.setState({searchTerm: value})
+        }
+
+        if(value.length===12) {
+            // trigger search run.
+            this.setState({searchLoading: true})
         }
     }
 
@@ -21,15 +31,30 @@ class Home extends Component {
         return (
             <div style={{textAlign: 'center', maxWidth: 500}}>
                 <img src={logo} />
-                <Input 
-                    size='massive' 
-                    icon='search' 
-                    placeholder='Find your EOS name...' 
-                    onChange={this.onSearchChange}
-                    maxLength={12}
-                    style={{backgroundColor: 'transparent'}}
-                    value={this.state.searchTerm}
-                />
+                <FadeIn delay={800}>
+
+                <Card fluid color='blue'>
+                    <Card.Content>
+                        <Card.Description>
+
+                            <Input 
+                                size='massive' 
+                                icon='search' 
+                                placeholder='Find your EOS name...' 
+                                onChange={this.onSearchChange}
+                                maxLength={12}
+                                style={{backgroundColor: 'transparent', marginBottom: '0.25em'}}
+                                value={this.state.searchTerm}
+                                loading={this.state.searchLoading}
+                            />
+                            <br />
+                            <span>{this.state.searchTerm.length} characters so far, 12 required.</span>
+                        </Card.Description>
+                    </Card.Content>
+                </Card>
+            </FadeIn>
+            <br /><br />
+            100% Open Source
             </div>
         );
     }
