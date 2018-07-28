22cf1b941acdb21cef8575f703ee73ec3638756a
Zach Alam <zelphics@gmail.com>
2018-07-27 19:48:46 -0700

    lower markup

--- nv-serverless/config/master.js
+++ nv-serverless/config/master.js
@@ -1,9 +1,8 @@
 'use strict';
 
 module.exports = {
-    standardMarkup: 3,          // how much extra in USD, to charge
+    standardMarkup: 2,          // how much extra in USD, to charge
     fallbackPricing: 99,        // how much to charge, when price is unavailable.
     requiredChars: 12,
     extraStake: 0.25           // how much EOS to offer in extra stake.
-
 }
