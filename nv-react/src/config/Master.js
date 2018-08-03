b09a9a9227d922c4ed52acfc2656accf9357f6ee
Zach Alam <zelphics@gmail.com>
2018-08-03 09:20:37 -0700

    set endpoint based on env

--- nv-react/src/config/Master.js
+++ nv-react/src/config/Master.js
@@ -1,4 +1,12 @@
+// production vars.
+let httpEndpoint = 'https://8p3f97b8si.execute-api.us-east-1.amazonaws.com/prod'
+
+// use the dev endpoint (and vars) if "localhost" appears in hostname
+if(window.location.hostname === "localhost") {
+    httpEndpoint = 'https://kfj0fl66oh.execute-api.us-east-1.amazonaws.com/dev'
+}
+
 export default {
-    httpEndpoint: 'https://8p3f97b8si.execute-api.us-east-1.amazonaws.com/prod',
+    httpEndpoint,
     requiredChars: 12
 }
